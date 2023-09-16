a89a2f33b2f09ffeb81ba6aea42f2b4c2da95b4d
Calebe94 <calebe94@disroot.org>
2023-09-16 20:56:29 -0300

    Added changes from serializers class

--- demo/views.py
+++ demo/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views import View
 from .models import Book
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer
 
 class Another(View):
     books = Book.objects.all()
@@ -20,3 +22,7 @@ class Another(View):
 def first(request):
     books = Book.objects.all()
     return render(request, 'first_temp.html', {'books': books})
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
