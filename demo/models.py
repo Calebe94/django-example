9d18d4c9ce512daf1c3d71666da097e9a6d03065
Calebe94 <calebe94@disroot.org>
2023-09-16 20:21:05 -0300

    Added changes from the model objects class

--- demo/models.py
+++ demo/models.py
@@ -4,6 +4,6 @@ class Book(models.Model):
     title = models.CharField(blank=False, unique=True, max_length=36)
     description = models.TextField(max_length=255, blank=True)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=3)
-    published = models.DateField(auto_now=True)
+    published = models.DateField(blank=True, null=True, default=None)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='covers/', blank=True)
--- demo/views.py
+++ demo/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 
 class Another(View):
+    books = Book.objects.all()
+    # books = Book.objects.filter(is_published=True)
+    # books = Book.objects.get(id=2)
+    # output = "We have this book {}".format(books.title )
+    output = "We have {} books in DB<br>".format(len(books))
+    output += "And they are:<br>"
+    for book in books:
+        output += "* {} - {} - {} - {}<br>".format(book.id, book.title, book.description, book.price)
+
     def get(self, request):
-        return HttpResponse("This is another function inside the 'Another' class")
-# Create your views here.
+        return HttpResponse(self.output)
